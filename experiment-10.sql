08c05d18b5e30651ab8dbf48e7652b5012b56d7f
dog-broad <rushyendra.7004@gmail.com>
2023-01-03 11:04:12 +0530

    Organize a few files and add a new trigger

--- experiment-10.sql
+++ experiment-10.sql
@@ -21,9 +21,6 @@ insert into sailors values(23,'Dustin',8,48.0);
 -- Check if trigger worked
 select * from sailors;
 
--- Reset tables:
-@C:\Users\STUDENT\Desktop\DBMS_Work\sailors.sql
-
 
 -- PL/SQL code for creation of trigger to update data into a table
 create or replace trigger t2
@@ -42,3 +39,19 @@ update sailors set sid=82 where rating=1;
 
 -- Check if trigger worked
 select * from sailors;
+
+
+-- PL/SQL code for creation of trigger to delete data from a table
+create or replace trigger t3
+after
+delete on sailors
+for each row
+begin
+if :old.sid = 22 then
+raise_application_error(-20019, 'you can not delete this row');
+end if;
+end;
+/
+
+-- Delete value to test
+Delete from sailors where sname='Dustin';
