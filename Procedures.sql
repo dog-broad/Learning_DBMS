d15a6b1c3e1384be865cd5e638faadad4df4081d
dog-broad <rushyendra.7004@gmail.com>
2023-01-10 10:05:47 +0530

    Add an execution check statemnt

--- Procedures.sql
+++ Procedures.sql
@@ -6,11 +6,16 @@
 
 create or replace procedure p_sail(sid1 in number)
 is
-v_sname varchar(10);
-v_age number(10);
+v_sname char(30);
+v_age float;
 begin
 select sname, age into v_sname, v_age from sailors where sid = sid1;
-dbms_output.put_line ('sname: '|| v_sname);
-dbms_output.put_line ('age: '|| v_age);
+dbms_output.put_line('sname: '|| v_sname);
+dbms_output.put_line('age: '|| v_age);
 end;
 /
+
+set serverOutput on;
+
+-- execute procedure
+execute p_sail(22);
