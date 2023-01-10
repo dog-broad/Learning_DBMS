95ea3ef8c7eb5ee43499b0582ff6081457f05ae6
dog-broad <rushyendra.7004@gmail.com>
2023-01-10 10:26:13 +0530

    Create an uodation Procedure

--- Procedures.sql
+++ Procedures.sql
@@ -19,3 +19,19 @@ set serverOutput on;
 
 -- execute procedure
 execute p_sail(22);
+
+-- SQL code for modification of a procedure on specified columns from a table
+
+create or replace procedure p_sailors(
+    v_sid in sailors.sid%type,
+    v_sname in sailors.sname%type,
+    v_age in sailors.age%type
+)is
+begin
+update sailors set sname = v_sname, age = v_age where sid = v_sid;
+commit;
+end;
+/
+
+-- execute procedure
+execute p_sailors(29, 'Ajit', '19');
