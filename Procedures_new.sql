8fc06d36ab8476fdc03f1dc5a56b1f20b1a2c18f
dog-broad <rushyendra.7004@gmail.com>
2023-01-24 11:02:33 +0530

    Cursors??

---
+++ Procedures_new.sql
@@ -0,0 +1,17 @@
+set serveroutput on;
+
+
+declare
+c_id customer.id%type;
+c_name customer.name%type;
+cursor c1 is
+select id, name from customer;
+begin
+open c1;
+loop
+fetch c1 into c_id, c_name; EXIT WHEN C1%NOTFOUND;
+dbms_output.put_line(c_id||''||c_name);
+end loop;
+close c1;
+end;
+/
